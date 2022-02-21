afdb7919ff43656fdc62b71dd12e7397944934ce
denniemate <wjdckdcjd@gmail.com>
2022-02-21 21:58:41 +0900

    알고리즘

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,41 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int[] arr;
-		
-		int testcase = in.nextInt();
-		
-		for(int i = 0 ; i < testcase ; i++) {
- 
-			int N = in.nextInt();
-			arr = new int[N];
-			
-			double sum = 0;
-			
-		
-			for(int j = 0 ; j < N ; j++) {
-				int val = in.nextInt();	
-				arr[j] = val;
-				sum += val;	
-			}
-			
-			double mean = (sum / N) ;
-			double count = 0; 
-			
-	
-			for(int j = 0 ; j < N ; j++) {
-				if(arr[j] > mean) {
-					count++;
-				}
-			}
-			
-			System.out.printf("%.3f%%\n",(count/N)*100);
-			
-	
-		}
-		in.close();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int i =0;
+while(true) {
+
+	i++;if(
+	a + (b*i) < c*i) break;
+}
+
+		System.out.println(i);
 	}
-	
 }
