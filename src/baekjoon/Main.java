f032daa984435fcfcca352d22c04acd0b7aabd3e
denniemate <wjdckdcjd@gmail.com>
2022-03-15 20:33:29 +0900

    알고리즘 공부

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,24 +1,33 @@
 package baekjoon;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
+	public static boolean prime[];
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-int x =1;
-int y= 3;
-		
-	}
-	public static int hanoi(int N,int x,int y) {
-		if(y>0) {
-			x = y;
-			y = 6 - x- y;
-			return hanoi(N,y,x%y);
-		}else
-		return 1;
+		Scanner sc =new Scanner(System.in);
+		int A[][] = new int [4][2];
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<2;j++) {
+				A[i][j]= sc.nextInt();
+			}
+		}
+		if(A[0][0] == A[1][0])
+			A[3][0] =A[2][0];
+		else if(A[0][0] == A[2][0])
+			A[3][0]=A[1][0];
+		else if(A[1][0] == A[2][0])
+			A[3][0]=A[0][0];
+		if(A[0][1] == A[1][1])
+			A[3][1]=A[0][1];
+		else if(A[0][1] == A[2][1])
+			A[3][1]=A[1][1];
+		else if(A[1][1] == A[2][1])
+			A[3][1]=A[0][1];
+		System.out.println(A[3][0]+" "+A[3][1]);
 	}
 }
