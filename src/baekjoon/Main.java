b1c12dadb05645194a7e07d4f7a11e431db4a210
denniemate <wjdckdcjd@gmail.com>
2022-03-21 23:02:26 +0900

    프로그래머스 알고리즘

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,30 +6,16 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		int n = 3;
-		int m = 12;
-		int[] answer = new int[2];
-		if (n > m) {
-			if (n % m == 0) {
-				answer[0] = m;
-				answer[1] = n;
-			} else {
-				answer[0] = 1;
-				answer[1] = n * m;
-			}
-		} else if (n < m) {
-			if (m % n == 0) {
-				answer[0] = n;
-				answer[1] = m;
-			} else {
-				answer[0] = 1;
-				answer[1] = n * m;
-			}
-		}
-		for (int i = 0; i < 2; i++) {
-			System.out.println(answer[i]);
-		}
-
+	public static void main(String[] args)  {
+	long n = 123456789;
+	String s = String.valueOf(n);
+	
+	StringBuilder sb = new StringBuilder(s);
+	sb.reverse();
+	String[] Arr = sb.toString().split("");
+	int[] answer= new int[sb.length()];
+	for(int i=0;i<sb.length();i++) {
+		answer[i] = Integer.parseInt(Arr[i]);
+	}
 	}
 }
