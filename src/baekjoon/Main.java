668f57deebf5f0d0dd2e5a2524e1f314a3709ed1
denniemate <wjdckdcjd@gmail.com>
2022-03-29 23:55:37 +0900

    알고리즘 공부

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,18 +1,19 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		String new_id = "...!@BaT#*..y.abcdefghijklm";
-		new_id=new_id.toLowerCase();
-		System.out.println(new_id);
+	public static void main(String[] args) {
+		String answer = "";
+		String s = "3people unFollowed me";
+		s.toLowerCase();
+		StringTokenizer st = new StringTokenizer(s, " ");
+		while (st.hasMoreTokens()) {
+			answer += st.nextToken();
+			if (!Character.isDigit(answer.charAt(0)))
+				System.out.println(answer.charAt(0));
+		}
+		System.out.println(answer);
 	}
 }
