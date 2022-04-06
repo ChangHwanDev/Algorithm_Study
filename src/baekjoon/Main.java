54b1d5b389cd2b847f4d6334589695f8e423f9f1
denniemate <wjdckdcjd@gmail.com>
2022-04-06 23:29:11 +0900

    파이썬 구현

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,40 +1,39 @@
 package baekjoon;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = Integer.parseInt(sc.nextLine());
-		int[][] map = new int[N][N];
-		int a = 1;
-		int b = 1;
-		while (N-- > 0) {
-			System.out.println("좌표입력");
-			if (sc.nextLine().equals("R")) {
-				b += 1;
-				continue;
-			}
-			if (sc.nextLine().equals("L")) {
-				if (b > 1)
-					b -= 1;
-				continue;
-			}
-			if (sc.nextLine().equals("U")) {
-				if (a == 1) {
-					continue;
-				} else {
-					a -= 1;
-					continue;
-				}
-			}
-			if (sc.nextLine().equals("D")) {
-				a += 1;
-				continue;
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int map[][] = new int[n][m];
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int count = 0;
+		int look = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				map[i][j] = sc.nextInt();
 			}
 		}
-		System.out.println(a + "" + b);
+		while (true) {
+			if (look != 3) {
+				look = 3;
+				if (x != 1) {
+					x -= 1;
+					count++;
+				}
+				if (x == 1) {
+					x += 1;
+					count++;
+				}
+			}
+			if (y == 1 && x == 4)
+				break;
+		}
+
+		System.out.println(count);
 	}
 }
