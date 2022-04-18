d02e9e7ef75ee1719eab28e5b38adec13dd56931
denniemate <wjdckdcjd@gmail.com>
2022-04-18 23:24:10 +0900

    test

--- 스프링공부깃허브/day19/item.js
+++
@@ -1,8 +0,0 @@
-function itemClickHandler(event) {
-    let target = event.target
-    while(target.classList.contains('item') == false) {
-        target = target.parentNode
-    }
-    console.log(target)
-    location.href = '/product/view/' + target.dataset.id
-}
