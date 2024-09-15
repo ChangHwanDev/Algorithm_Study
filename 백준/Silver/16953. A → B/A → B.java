220b97ca9098e697304b0f78bd4e13659530b013
denniemate <93104447+denniemate@users.noreply.github.com>
2024-09-16 00:01:52 +0900

    [Silver II] Title: A → B, Time: 132 ms, Memory: 21192 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -4,27 +4,39 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static long min = Integer.MAX_VALUE;
-    static boolean[] visited;
-    static int m;
+    static long answer;
+    static long n, m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        visited = new boolean[m + 1];
-        dfs(n, 1);
-        long answer = min == Integer.MAX_VALUE ? -1 : min;
-        System.out.println(answer);
+
+        bfs();
     }
 
-    static void dfs(long v, long level) {
-        if (v > m) return;
-        if (v == m) {
-            if (min > level) {min = level;}
-            return;
+    static void bfs() {
+        Queue<Long> q = new LinkedList<>();
+        answer = 0;
+        q.add(n * 10 + 1);
+        q.add(n * 2);
+
+        while(!q.isEmpty()) {
+            answer++;
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                long now = q.poll();
+                if(now > m) continue;
+                if(now == m) {
+                    System.out.println(answer+1);
+                    return;
+                }
+                q.add(now*2);
+                q.add(now*10+1);
+            }
         }
-        if(v * 10 + 1 <= m) dfs(v * 10 + 1, level + 1);
-        if(v * 2 <= m) dfs(v * 2, level + 1);
+
+        System.out.println(-1);
     }
 }
