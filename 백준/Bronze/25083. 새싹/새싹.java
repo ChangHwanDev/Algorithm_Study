3d147861c3d66886b1d8e193cd357f3ebe4312b0
denniemate <93104447+denniemate@users.noreply.github.com>
2024-09-16 12:53:32 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.println(s);
+    }
+}
