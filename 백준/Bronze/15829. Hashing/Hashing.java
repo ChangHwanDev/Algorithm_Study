5d00c00666b6fabbbbbdd21872e5cd46c83a4e87
denniemate <93104447+denniemate@users.noreply.github.com>
2024-09-16 14:36:42 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14236 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int answer = 0;
+        int pow = 1;
+        for (int i = 0; i < n; i++) {
+            answer += (s.charAt(i) - 96) * pow;
+            pow = pow * 31 % 1234567891;
+        }
+        System.out.println(answer % 1234567891);
+    }
+}
