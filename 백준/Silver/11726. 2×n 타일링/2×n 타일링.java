ae8803464ea5804459ae7761e49afb15f05ae71c
denniemate <93104447+denniemate@users.noreply.github.com>
2024-09-16 17:41:30 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        dp = new int[n + 1];
+
+
+        int answer = solve(n);
+        System.out.println(answer);
+    }
+    public static int solve(int n) {
+        if(n == 1) return 1;
+        else if (n == 2) return 2;
+
+        if(dp[n] != 0) return dp[n];
+        else return dp[n] = (solve(n - 1) + solve(n - 2)) % 10007;
+    }
+}
