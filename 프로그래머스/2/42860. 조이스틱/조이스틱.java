074cb39a158ccc80f9abf8b5bf64d70aedfffc29
denniemate <93104447+denniemate@users.noreply.github.com>
2024-10-04 20:09:50 +0900

    [level 2] Title: 조이스틱, Time: 0.12 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String name) {
+            int answer = 0;
+            int length = name.length();
+
+            int idx;
+            int move = length - 1;
+
+            for(int i = 0; i < name.length(); i++){
+                answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+                idx = i + 1;
+                while(idx < length && name.charAt(idx) == 'A'){
+                    idx++;
+                }
+
+                move = Math.min(move, i * 2 + length - idx);
+                move = Math.min(move, (length - idx) * 2 + i);
+            }
+            return answer + move;
+    }
+}
