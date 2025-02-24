596ba26af59562b81f95f3d8e8a415bbe33a3abd
ChangHwan Jung <93104447+ChangHwanDev@users.noreply.github.com>
2025-02-24 22:01:09 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 15688 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 1;
+        dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    result = Math.max(result, dp[i]);
+                }
+            }
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append(result).append("\n");
+
+       for (int i = N -1; i >= 0; i--) {
+           if (dp[i] == result) {
+               result -= 1;
+               stack.push(arr[i]);
+           }
+       }
+
+       while (!stack.isEmpty()) {
+           sb.append(stack.pop()).append(" ");
+       }
+
+        System.out.println(sb.toString());
+    }
+}
