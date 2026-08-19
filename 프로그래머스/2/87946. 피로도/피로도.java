617b053d6ac968fff9acd9fcbb9df2937718766d
ChangHwan Jung <93104447+ChangHwanDev@users.noreply.github.com>
2026-08-20 00:20:32 +0900

    [level 2] Title: 피로도, Time: 1.53 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,24 @@
 class Solution {
-        static boolean[] visited;
-        static int answer;
-    public int solution(int k, int[][] dungeons) {
-            answer = 0;
-            visited = new boolean[dungeons.length];
+    static boolean[] visited;
+    static int answer;
 
-            dfs(k, dungeons, 0);
+    public int solution(int k, int[][] dungeons) {
+        answer = 0;
+        visited = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
         return answer;
     }
     
-    public static void dfs(int score, int[][] dungeons, int level){
-            answer = Math.max(answer, level);
+    public static void dfs(int score, int[][] dungeons, int level) {
+        answer = Math.max(answer, level);
 
-            for (int j = 0; j < dungeons.length; j++) {
-                if (!visited[j]) {
-                    if (dungeons[j][0] <= score) {
-                        visited[j] = true;
-                        dfs(score - dungeons[j][1], dungeons, level + 1);
-                        visited[j] = false;
-                    }
-                }
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= score) {
+                visited[i] = true;
+                dfs(score - dungeons[i][1], dungeons, level + 1);
+                visited[i] = false;
             }
         }
+    }
 }
